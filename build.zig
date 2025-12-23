165ace01bb7855b80559342cd6ef76a9ab09dac1
ebits <tspamiitesh@gmail.com>
2025-12-23 19:43:54 +0530

    Changes in build.zig for interoping readline.h

--- build.zig
+++ build.zig
@@ -2,26 +2,30 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable(.{
-        .name = "fastsh",                
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/main.zig"),
-            .target = target,
-            .optimize = optimize,
-        })
+    const exe_mod = b.createModule(.{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
     });
+    exe_mod.linkSystemLibrary("readline", .{});
 
+    const exe = b.addExecutable(.{
+        .name = "fastsh",
+        .root_module = exe_mod,
+    });
     b.installArtifact(exe);
 
     const run_cmd = b.addRunArtifact(exe);
-    run_cmd.step.dependOn(b.getInstallStep());  
     if (b.args) |args| {
-        run_cmd.addArgs(args);  
+        run_cmd.addArgs(args);
     }
+    b.step("run", "Run the app").dependOn(&run_cmd.step);
 
-    const run_step = b.step("run", "Run the shell program");
-    run_step.dependOn(&run_cmd.step);
+    const unit_tests = b.addRunArtifact(b.addTest(.{
+        .root_module = exe_mod,
+    }));
+    b.step("test", "Run unit tests").dependOn(&unit_tests.step);
 }
